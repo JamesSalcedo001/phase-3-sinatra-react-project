358c91966c03d85934904db974d6b94fa29937c6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-23 17:02:15 -0500

    used db migrate to create the cats table

--- db/migrate/20230323215209_create_cats.rb
+++ db/migrate/20230323215209_create_cats.rb
@@ -1,4 +1,12 @@
 class CreateCats < ActiveRecord::Migration[6.1]
   def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+      t.string :descr
+      t.string :image
+      t.integer :shelter_id
+    end
   end
 end
