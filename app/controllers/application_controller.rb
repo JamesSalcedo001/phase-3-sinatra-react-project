7e7fb695b627f93299e0b1af34c74c7cfcaaf6a5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-04-06 13:55:09 -0500

    added an alternate route to control data included in cats array

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  # get "/cats" do
+  #   all_cats = Cat.all
+  #   all_cats.to_json(include: :shelter)
+  # end
+
   get "/cats" do
     all_cats = Cat.all
-    all_cats.to_json(include: :shelter)
+    all_cats.to_json(include: {shelter: { only: [:name, :location, :employees, :image] } } )
   end
 
   get "/cats/:id" do
